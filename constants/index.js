3da980bc996c8b9e2d8f6194d8811ceb2cf6bed9
Prabodana Miyuranga <150063011+miyuranga-dev@users.noreply.github.com>
2026-03-31 13:06:45 +0530

    implement the first part of the ui

---
+++ constants/index.js
@@ -0,0 +1,21 @@
+const navLinks = [
+ {
+	id: "cocktails",
+	title: "Cocktails",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "The Art",
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+]
+
+
+export { navLinks };
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import './App.css'
 import gsap from 'gsap';
+import Navbar from './components/Navvar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,9 +10,11 @@ function App() {
  
 
   return (
-    <div className='flex-center h-screen'>
-      <h1 className='text-3xl text-indigo-300'>Hello Gsap</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
+    </main>
   )
 }
 
